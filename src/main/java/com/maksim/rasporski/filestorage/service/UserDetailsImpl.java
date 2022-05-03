de8994d2b9698dfa541d0180f756e465077d2f26
Распорский Максим <rolk473@gmail.com>
2022-05-03 16:37:10 +0300

    Implement UserDetailsService and UserDetails

---
+++ src/main/java/com/maksim/rasporski/filestorage/service/UserDetailsImpl.java
@@ -0,0 +1,62 @@
+package com.maksim.rasporski.filestorage.service;
+
+import com.maksim.rasporski.filestorage.entity.AppUser;
+import com.maksim.rasporski.filestorage.entity.UserRoles;
+import com.maksim.rasporski.filestorage.logic.Roles;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * @author Maksim Rasporski
+ * @link https://vk.com/maximrasporsky
+ */
+public class UserDetailsImpl implements UserDetails {
+    private final String username;
+    private final String password;
+    private Set<GrantedAuthority> userRoles;
+
+    public UserDetailsImpl(final AppUser user) {
+        username = user.getEmail().toLowerCase();
+        password = user.getPassword();
+        userRoles.add(new SimpleGrantedAuthority(user.getRoles().getRole().toUpperCase()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userRoles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/maksim/rasporski/filestorage/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.maksim.rasporski.filestorage.service;
+
+import com.maksim.rasporski.filestorage.entity.AppUser;
+import com.maksim.rasporski.filestorage.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * @author Maksim Rasporski
+ * @link https://vk.com/maximrasporsky
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        AppUser currentUser = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with username %s not found", username)));
+        return new UserDetailsImpl(currentUser);
+    }
+}
